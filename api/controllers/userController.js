72002a27d76512bb3a25d5826717827236d3a621
Florent Paris <florentparis28@gmail.com>
2023-03-16 10:56:54 +0100

    fix endpoint

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,31 +1,26 @@
-import connect_db from "../test.js";
-const conn = connect_db();
-console.log(conn);
+import pool from '../config/db.js';
+
 export function login(req, res) {
   const { email, password } = req.body;
-  connect_db().query(`SELECT email FROM users WHERE email=${email} AND password=${password}`, async (error, res) => {
-    if( result.length > 0 ) {
-      return res.render('login', {
-        message: 'Connection failed'
-      })
-    }
-  })
+  pool
+    .query(
+      `SELECT email FROM users WHERE email=${email} AND password=${password}`
+    )
+    .then((response, error) => {
+      if (error) {
+        res.send(error);
+      }
+      if (response.length > 0) {
+        return res.render('login', {
+          message: 'Connection failed',
+        });
+      }
+    });
 }
 
 export function register(req, res) {
   const { lastName, firstName, email, password, password_confirm } = req.body;
-  /*connect_db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "Select * from Users";
-    connect_db.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
+  pool.query('SELECT * from Users').then((response) => {
+    res.send(response);
   });
-   */
-  conn.query("SELECT * FROM Users", async (error, res) => {
-    console.log(res)
-  });
-  conn.end();
 }
